04934542919adf7b9dfb7a1339a395b449434ef3
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-03-21 20:14:25 -0300

    feat: adiciona validação de conflito de horário no agendamento

--- src/main/java/com/java/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/com/java/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
@@ -3,9 +3,10 @@ package com.java.agendador_horarios.infrastructure.repository;
 import com.java.agendador_horarios.infrastructure.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-
-
+    Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHora, LocalDateTime dataFinal);
 
 
 
--- src/main/java/com/java/agendador_horarios/services/AgendamentoService.java
+++ src/main/java/com/java/agendador_horarios/services/AgendamentoService.java
@@ -1,15 +1,34 @@
 package com.java.agendador_horarios.services;
 
+import com.java.agendador_horarios.infrastructure.entity.Agendamento;
 import com.java.agendador_horarios.infrastructure.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class AgendamentoService {
 
-    private final JpaRepository<AgendamentoRepository, Long> repository;
+    private final AgendamentoRepository agendamentoRepository;
+
+    public  Agendamento salvarAgendamento(Agendamento agendamento) {
+
+        LocalDateTime horaAgendamento = agendamento.getDataHoraAgendamento();
+        LocalDateTime horaFim = agendamento.getDataHoraAgendamento().plusHours(1);
+
+        Agendamento agendados = agendamentoRepository.findByServicoAndDataHoraAgendamentoBetween(agendamento.getServico(), horaAgendamento, horaFim);
 
 
+        if(Objects.nonNull(agendados)) {
+            agendamentoRepository.save(agendamento);
+        }
+        return  agendamentoRepository.save(agendados);
+
+    }
+
+    public void deletarAgendamento(String cliente, LocalDateTime dataHoraAgendamento) {
+    }
 }
