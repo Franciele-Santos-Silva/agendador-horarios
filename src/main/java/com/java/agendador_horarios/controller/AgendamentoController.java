f57616e0bd4352775c6506366e59cbf9d40b249d
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-04-05 14:39:46 -0300

    feat: adicionar mapeamento base /agendamentos no controller

--- src/main/java/com/java/agendador_horarios/controller/AgendamentoController.java
+++ src/main/java/com/java/agendador_horarios/controller/AgendamentoController.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController
+@RestController("/agendamentos")
 @RequiredArgsConstructor
 public class AgendamentoController {
 
